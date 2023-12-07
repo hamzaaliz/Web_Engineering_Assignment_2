18a45bc1059042c159584bac16a72f9e51365587
hamzaaliz1008 <hali.bese21seecs@seecs.edu.pk>
2023-12-07 19:31:46 +0500

    Did some modifications to index.php

--- index.php
+++ index.php
@@ -1,11 +1,33 @@
 <?php
+$url_queue = [];
+$url_responses_queue = [];
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $seedURL = $_POST["url"];
     $depth = $_POST["depth"];
 
-    echo "URL: $seedURL<br>";
-    echo "Depth: $depth";
-} else {
+    global $url_queue;
+    $url_queue[] = $seedURL;
+    saveHTMLContent($seedURL);
+} 
+else {
     echo "Form not submitted.";
 }
+
+// This function takes a URL as an input, sends an HTTP request to that URL, saves the response in a variable, and appends the response to a global array.
+function saveHTMLContent($url) {
+    global $url_responses_queue;
+
+    $response = file_get_contents($url);
+
+    if ($response === FALSE) {
+        $url_responses_queue[] = "ERROR";
+    } 
+    else {
+        $url_responses_queue[] = $response;
+    }
+}
+
 ?>
+
+
