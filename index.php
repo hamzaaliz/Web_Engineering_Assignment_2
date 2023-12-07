29c13d0228a31927a2c75c98573145eb6fa4c069
hamzaaliz1008 <hali.bese21seecs@seecs.edu.pk>
2023-12-07 14:22:14 +0500

    Created index.html, and updated index.php

--- index.php
+++ index.php
@@ -6,15 +6,6 @@
     <title>Web Spider</title>
 </head>
 <body>
-    <form action="index.php" method="post">
-        <label for="url">Enter the Seed URL:</label>
-        <input type="url" id="url">
-        <label for="depth">Enter the depth of searching:</label>
-        <input type="number" id="depth">
-    </form>
-</body>
-</html>
-
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $seedURL = $_POST["url"];
@@ -26,3 +17,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     echo "Form not submitted.";
 }
 ?>
+<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+    <label for="url">Enter the Seed URL:</label>
+    <input type="url" id="url">
+    <label for="depth">Enter the depth of searching:</label>
+    <input type="number" id="depth">
+    <input type="submit"> 
+</form>
+</body>
+</html>
