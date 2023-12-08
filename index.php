768a4a3970c4abca7eca476042af1ee5f03ae1ea
hamzaaliz1008 <hali.bese21seecs@seecs.edu.pk>
2023-12-08 23:35:28 +0500

    Finished coding

--- index.php
+++ index.php
@@ -4,44 +4,107 @@
 
 $url_queue = [];
 $url_responses_queue = [];
+$choice = '';
 
 // Receiving form data from index.html, and adding the seed URL to the queue 
-// and calling saveHTMLContent() to save it's HTML content via a HTTP request/response cycle.
+// and calling checkAndAppendHTMLResponse() to save its HTML content via an HTTP request/response cycle.
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $seedURL = $_POST["url"];
-    $depth = $_POST["depth"];
 
-    if (isValidURL($seedURL)) {
-        $url_queue[] = $seedURL;
-    } else {
-        echo "The provided Seed URL is invalid.";
+    // Check if the URL is allowed by robots.txt
+    if (!isUrlAllowedByRobotsTxt($seedURL)) {
+        echo "Crawling is not allowed for this URL based on robots.txt rules.";
         exit;
     }
-    saveHTMLContent($seedURL);
+
+    if (isset($_POST['task'])) {
+        $choice = $_POST['task'];
+    }
+
+    if ($choice === 'find') {
+        $searchText = $_POST["searchText"];
+        searchContent($searchText);
+    }
+
+    if (isURLValid($seedURL) && checkAndAppendHTMLResponse($seedURL)) {
+        $url_queue[] = $seedURL;
+
+        extractURLs($url_responses_queue[0]);
+
+        for ($i = 1; $i < count($url_queue); $i++) {
+            checkAndAppendHTMLResponse($url_queue[$i]);
+        }
+
+        if ($choice === 'view') {
+            for ($i = 0; $i < count($url_responses_queue); $i++) {
+                viewCrawledContent($url_responses_queue[$i]);
+            }
+        }
+    } else {
+        echo "The provided Seed URL is invalid or there was no response from that URL.";
+        exit;
+    }
 } else {
     echo "Form not submitted.";
 }
 
-// This function takes a URL as an input, sends an HTTP request to that URL, 
-// saves the response in a variable, and appends the response to a global array.
+// Function to check if a URL is allowed by robots.txt
+function isUrlAllowedByRobotsTxt($url) {
+    $robotsTxtUrl = rtrim($url, '/') . '/robots.txt';
+    $robotsTxtContent = @file_get_contents($robotsTxtUrl);
 
-function saveHTMLContent($url) {
+    if ($robotsTxtContent !== false) {
+        // Parsing the content of robots.txt and checking if the URL is allowed.
+        return isUrlAllowedByRobotsTxtContent($robotsTxtContent);
+    }
+
+    // If unable to fetch robots.txt, assume the URL is allowed
+    return true;
+}
+
+// Function to parse robots.txt content and check if a URL is allowed
+function isUrlAllowedByRobotsTxtContent($content) {
+    $lines = explode("\n", $content);
+    
+    foreach ($lines as $line) {
+        $line = trim($line);
+
+        if (strpos($line, 'Disallow:') === 0) {
+            $disallowedPath = trim(substr($line, strlen('Disallow:')), " \t\n\r\0\x0B/");
+            
+            // Check if the URL matches the disallowed path
+            if (strpos($_POST["url"], $disallowedPath) === 0) {
+                return false;
+            }
+        }
+    }
+
+    // If no Disallow directive matched, assume the URL is allowed
+    return true;
+}
+
+// Function to check if a URL is valid
+function isURLValid($url) {
+    return filter_var($url, FILTER_VALIDATE_URL) !== false;
+}
+
+// Function to check and append HTML response to the queue
+function checkAndAppendHTMLResponse($url) {
     global $url_responses_queue;
 
     $response = file_get_contents($url);
 
     if ($response === FALSE) {
-        $url_responses_queue[] = "ERROR: Failed to fetch content from $url";
+        $url_responses_queue[] = "ERROR";
+        return false;
     } else {
         $url_responses_queue[] = $response;
-        extractURLs($response);
+        return true;
     }
 }
 
-// This function takes HTML content, which is an HTTP response from a URL,
-// and extracts all the URLs in that HTML and appends them to the queue of URLs.
-
+// Function to extract URLs from HTML content and add them to the queue
 function extractURLs($url_response) {
     global $url_queue;
 
@@ -55,20 +118,49 @@ function extractURLs($url_response) {
             $url_queue[] = $node->getAttribute('href');
         }
     } else {
-        $url_responses_queue[] = "ERROR: Invalid HTML response.";
+        $url_responses_queue[] = "ERROR";
     }
 }
 
-// This function takes a URL as an input, and returns true if the URL is valid, else false.
+// Function to view crawled content
+function viewCrawledContent($html) {
+    $dom = new DOMDocument;
+    libxml_use_internal_errors(true); 
+    $dom->loadHTML($html);
+    libxml_clear_errors();
 
-function isValidURL($url) {
-    return filter_var($url, FILTER_VALIDATE_URL) !== false;
+    $title = $dom->getElementsByTagName('title')->item(0)->nodeValue;
+
+    $metaTags = $dom->getElementsByTagName('meta');
+    $metaInfo = [];
+
+    foreach ($metaTags as $metaTag) {
+        $name = $metaTag->getAttribute('name');
+        $content = $metaTag->getAttribute('content');
+        $metaInfo[$name] = $content;
+    }
+
+    echo "Title: $title <br />Meta Information: $metaInfo<hr />";
 }
 
-// for ($i = 0; $i < count($url_queue); $i++) {
-//     echo "URL: " . $url_queue[$i] . "<br>";
-//     echo "<hr>";
-// }
+// Function to search content for a specified string
+function searchContent($searchText) {
+    global $url_responses_queue;
+
+    foreach ($url_responses_queue as $url_response) {
+        if (strpos($url_response, $searchText) !== false) {
+            echo "Found in URL: " . getCurrentURL() . "<br>";
+            echo "Searched Text: $searchText <br>";
+        }
+    }
+}
+
+// Function to get the current URL from the URL queue
+function getCurrentURL() {
+    global $url_queue;
+    $currentIndex = count($url_queue) - 1;
+    return $url_queue[$currentIndex];
+}
 
 echo "Total URLs in the queue: " . count($url_queue);
 echo "Total Responses in the queue" . count($url_responses_queue);
